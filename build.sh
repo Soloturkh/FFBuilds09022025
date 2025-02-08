0d3a7faf2f335c6af11c18d2d4f2d759bc17d26a
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2025-02-09 02:17:24 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,6 +31,8 @@ cat <<EOF >"$BUILD_SCRIPT"
     set -xe
     cd /ffbuild
     rm -rf ffmpeg prefix
+    
+    apt-get update && apt-get install -y zlib1g-dev libexpat1-dev
 
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
